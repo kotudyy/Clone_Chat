275f4ede2ed79163f34a44e9bd5625ea6afcdf0b
grusie <grusie@github.com>
2021-11-26 14:55:20 +0900

    Authentication

--- app/src/main/java/com/example/chatting/AuthActivity.kt
+++ app/src/main/java/com/example/chatting/AuthActivity.kt
@@ -2,6 +2,7 @@ package com.example.chatting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.chatting.databinding.ActivityAuthBinding
 
@@ -23,6 +24,7 @@ class AuthActivity : AppCompatActivity() {      //회원가입 액티비티
                     MyApplication.auth.currentUser?.sendEmailVerification()?.addOnCompleteListener {
                             sendTask -> if(sendTask.isSuccessful){      //이메일 전송
                         Toast.makeText(this,"회원가입에 성공했습니다. 전송된 메일을 확인해 주세요.", Toast.LENGTH_SHORT).show()
+                        Log.d("Grusie","${sendTask.exception}")
                     }else {
                         Toast.makeText(this,"메일 전송 실패", Toast.LENGTH_SHORT).show()
                     }
