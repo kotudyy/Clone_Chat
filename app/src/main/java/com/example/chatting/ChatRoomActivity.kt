9b3544f3019541a8ba4b49ef14438058eca5a754
Jiyeon Choi <cwd6372@gmail.com>
2021-11-26 21:28:16 +0900

    add ChatRoomActivity

---
+++ app/src/main/java/com/example/chatting/ChatRoomActivity.kt
@@ -0,0 +1,52 @@
+package com.example.chatting
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MenuItem
+import com.example.chatting.databinding.ActivityChatRoomBinding
+
+class ChatRoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityChatRoomBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.etMessage.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if (binding.etMessage.length() > 0) {
+                    binding.btnSend.setBackgroundColor(applicationContext.resources.getColor(R.color.yellow))
+                    binding.btnSend.isEnabled = true
+                } else {
+                    binding.btnSend.setBackgroundColor(Color.WHITE)
+                    binding.btnSend.isEnabled = false
+                }
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (binding.etMessage.length() > 0) {
+                    binding.btnSend.setBackgroundColor(applicationContext.resources.getColor(R.color.yellow))
+                    binding.btnSend.isEnabled = true
+                } else {
+                    binding.btnSend.setBackgroundColor(Color.WHITE)
+                    binding.btnSend.isEnabled = false
+                }
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (binding.etMessage.length() > 0) {
+                    binding.btnSend.setBackgroundColor(applicationContext.resources.getColor(R.color.yellow))
+                    binding.btnSend.isEnabled = true
+                } else {
+                    binding.btnSend.setBackgroundColor(Color.WHITE)
+                    binding.btnSend.isEnabled = false
+                }
+            }
+        })
+
+
+    }
+}
--- app/src/main/java/com/example/chatting/MainActivity.kt
+++ app/src/main/java/com/example/chatting/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.chatting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatting.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainbtn.setOnClickListener {
+            val intent: Intent = Intent(this, ChatRoomActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
