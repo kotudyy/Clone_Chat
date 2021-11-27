489462fb99d18899689a61acd1fa424b27b84afe
dongk00 <dddongk00@gmail.com>
2021-11-27 10:43:33 +0900

    ChatListActivity에 User,Chat Fragment 배치

--- app/src/main/java/com/example/chatting/MainActivity.kt
+++ app/src/main/java/com/example/chatting/MainActivity.kt
@@ -1,27 +1,49 @@
 package com.example.chatting
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
+import com.example.chatting.ChatFragment.ChatFragment
+import com.example.chatting.UserFragment.UsersFragment
 import com.example.chatting.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        setContentView(binding.root)
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.auth_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        startActivity(Intent(this, LoginActivity::class.java))
-        return super.onOptionsItemSelected(item)
-    }
+        val transaction = supportFragmentManager.beginTransaction()
+            .add(R.id.mainFrameLayout, ChatFragment())
+            .commit()
 
+        binding.tabLayout.selectTab(binding.tabLayout.getTabAt(1))
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab!!.position){
+                    0 -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.mainFrameLayout, UsersFragment())
+                            .commit()
+                    }
+
+                    1 -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.mainFrameLayout, ChatFragment())
+                            .commit()
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // 최상단으로 스크롤
+            }
+
+        })
+    }
 }
