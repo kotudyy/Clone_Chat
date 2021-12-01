cdc7fe2e3f3727f2dbee71148cf6ae262a9f70ac
Jiyeon Choi <cwd6372@gmail.com>
2021-12-01 09:48:10 +0900

    ChatListActivity 수정 및 ChatRoomActivity 인텐트 연결

--- app/src/main/java/com/example/chatting/ChatFragment/RvItemChatAdapter.kt
+++ app/src/main/java/com/example/chatting/ChatFragment/RvItemChatAdapter.kt
@@ -1,29 +1,51 @@
 package com.example.chatting.ChatFragment
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.example.chatting.ChatRoomActivity
 import com.example.chatting.databinding.RvitemChatBinding
 
-class RvItemChatViewHolder(val binding: RvitemChatBinding): RecyclerView.ViewHolder(binding.root){
+class RvItemChatViewHolder(val binding: RvitemChatBinding) : RecyclerView.ViewHolder(binding.root) {
 
-    fun setData(data: ChatData){
-        binding.text1.text = "${data.text1}"
-        binding.text2.text = "${data.text2}"
+    fun setData(data: ChatData) {
+        binding.tvChatUsername.text = "${data.chatUsername}"
+        binding.tvChatPreviewmsg.text = "${data.chatPreviewMsg}"
+        binding.tvChatTimestamp.text = "${data.chatTime}"
     }
 }
 
-class RvItemChatAdapter(var chatData: MutableList<ChatData>): RecyclerView.Adapter<RvItemChatViewHolder>(){
+class RvItemChatAdapter(var chatData: MutableList<ChatData>) :
+    RecyclerView.Adapter<RvItemChatViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvItemChatViewHolder =
-        RvItemChatViewHolder(RvitemChatBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        RvItemChatViewHolder(
+            RvitemChatBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
 
     override fun onBindViewHolder(holder: RvItemChatViewHolder, position: Int) {
         val data = chatData[position]
         holder.setData(data)
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(holder.itemView?.context, ChatRoomActivity::class.java)
+            intent.putExtra("userName", data.chatUsername)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            ContextCompat.startActivity(holder.itemView.context, intent, null)
+        }
     }
 
     override fun getItemCount(): Int = chatData.size
 
 }
 
-data class ChatData(val text1: String, val text2: String)
+data class ChatData(
+    val chatUsername: String,
+    val chatPreviewMsg: String,
+    val chatTime: String,
+)
