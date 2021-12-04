c1866f205ed4f50c75adf666869caf38d2438e56
Jiyeon Choi <cwd6372@gmail.com>
2021-12-04 10:06:58 +0900

    Firebase 친구목록 받아오는 기능 구현

--- app/src/main/java/com/example/chatting/Login/AuthActivity.kt
+++ app/src/main/java/com/example/chatting/Login/AuthActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Toast
 import com.example.chatting.MyApplication
 import com.example.chatting.databinding.ActivityAuthBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AuthActivity : AppCompatActivity() {      //회원가입 액티비티
     lateinit var binding: ActivityAuthBinding
@@ -25,6 +26,24 @@ class AuthActivity : AppCompatActivity() {      //회원가입 액티비티
                     MyApplication.auth.currentUser?.sendEmailVerification()?.addOnCompleteListener {
                             sendTask -> if(sendTask.isSuccessful){      //이메일 전송
                         Toast.makeText(this,"회원가입에 성공했습니다. 전송된 메일을 확인해 주세요.", Toast.LENGTH_SHORT).show()
+
+                        // 회원가입 성공 시 profile 컬렉션에 회원 정보 저장
+                        val data = hashMapOf(
+                            "email" to email,
+                            "music" to "testmusic",
+                            "name" to "yeon",
+                            "status_message" to "teststatus"
+                        )
+                        FirebaseFirestore.getInstance().collection("profile")
+                            .add(data)
+                            .addOnSuccessListener {
+                                // 성공할 경우
+                                Toast.makeText(this, "데이터가 추가되었습니다", Toast.LENGTH_SHORT).show()
+                            }
+                            .addOnFailureListener { exception ->
+                                // 실패할 경우
+                                Log.w("MainActivity", "Error getting documents: $exception")
+                            }
                     }else {
                         Toast.makeText(this,"메일 전송 실패", Toast.LENGTH_SHORT).show()
                     }
--- app/src/main/java/com/example/chatting/UserFragment/UsersFragment.kt
+++ app/src/main/java/com/example/chatting/UserFragment/UsersFragment.kt
@@ -1,6 +1,7 @@
 package com.example.chatting.UserFragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,12 +9,19 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chatting.ChatFragment.RvItemChatAdapter
+import com.example.chatting.MyApplication
 import com.example.chatting.R
 import com.example.chatting.databinding.FragmentUsersBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.FirebaseFirestore
 
 class UsersFragment : Fragment() {
 
     lateinit var binding: FragmentUsersBinding
+    val userData = mutableListOf<UserData>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,15 +60,41 @@ class UsersFragment : Fragment() {
             }
         }
 
-        //RecyclerView DummyData
-        val dummyData = mutableListOf<UserData>()
-        for (i in 1..20) {
-            dummyData.add(UserData("Text", "$i$i"))
-        }
-
-        val adapter = RvItemUserAdapter(dummyData)
+        val adapter = RvItemUserAdapter(userData)
+        adapter.userData = userData
         binding.rvUser.adapter = adapter
         binding.rvUser.layoutManager = LinearLayoutManager(this.context)
+
+        val myEmail = MyApplication.auth.currentUser?.email.toString()
+
+        // 본인/친구 프로필 정보 가져오기
+        FirebaseFirestore.getInstance().collection("profile")
+            .get()      // 문서 가져오기
+            .addOnSuccessListener { result ->
+                // 성공할 경우
+                userData.clear()
+                for (document in result) {  // 가져온 문서들은 result에 들어감
+                    if (document["email"] as String == myEmail) {   // 본인인 경우
+                        binding.Myname.text = document["name"] as String
+                        binding.statusMessage.text = document["status_message"] as String
+                        binding.chip4.text = document["music"] as String
+                    }
+                    else {
+                        val item = UserData(
+                            document["email"] as String, document["music"] as String,
+                            document["name"] as String, document["status_message"] as String
+                        )
+
+                        userData.add(item)
+                    }
+                }
+                adapter.notifyDataSetChanged()  // 리사이클러 뷰 갱신
+            }
+            .addOnFailureListener { exception ->
+                // 실패할 경우
+                Log.w("MainActivity", "Error getting documents: $exception")
+            }
+
         return binding.root
     }
 }
