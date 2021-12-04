d39f93e3ab343190e75283f0e3ff6f013eb33a0a
dongk00 <dddongk00@gmail.com>
2021-12-04 10:05:45 +0900

    UserData RV 출력 프로필 화면 수정 기능 일부

---
+++ app/src/main/java/com/example/chatting/Model/ProfileData.kt
@@ -0,0 +1,9 @@
+package com.example.chatting.Model
+
+data class ProfileData(
+    val email: String,
+    var name: String,
+    var image_url: String,
+    var statusMsg: String,
+    var statusMusic: String
+)
