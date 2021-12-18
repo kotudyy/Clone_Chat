89f9224b40870f3f804d895533205e21744f9d33
cjyeon <cwd6372@gmail.com>
2021-12-18 09:39:59 +0900

    채팅방리스트 불러오는 기능 수정

--- app/src/main/java/com/example/chatting/ChatFragment/ChatFragment.kt
+++ app/src/main/java/com/example/chatting/ChatFragment/ChatFragment.kt
@@ -1,5 +1,6 @@
 package com.example.chatting.ChatFragment
 
+import android.R.attr
 import android.os.Bundle
 import android.util.Log
 import android.view.*
@@ -8,6 +9,7 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.chatting.Model.UserData
 import com.example.chatting.Model.UserRoom
 import com.example.chatting.MyApplication
@@ -17,13 +19,21 @@ import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import android.R.attr.data
+import java.util.*
+import kotlin.Comparator
+
 
 class ChatFragment : Fragment() {
 
     lateinit var binding: FragmentChatBinding
     val chatListDatas = mutableListOf<UserRoom>()
     var userEmail = MyApplication.auth.currentUser?.email!!
-    val adapter = RvItemChatAdapter(chatListDatas)
+    var adapter = RvItemChatAdapter(chatListDatas)
+
+    var user1: String? = null
+    var user2: String? = null
+    var chatRoomId: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -69,8 +79,6 @@ class ChatFragment : Fragment() {
     }
 
     fun initDogRecyclerView() {
-//        시간 순으로 정렬하기
-//        chatListDatas = MutableList(chatListDatas.sortedBy { it.timestamp })
         adapter.chatData = chatListDatas
         binding.rvChat.adapter = adapter
         binding.rvChat.layoutManager = LinearLayoutManager(context) //레이아웃 매니저 연결
@@ -78,40 +86,56 @@ class ChatFragment : Fragment() {
 
     //채팅방 리스트 가져오기 searchChatRoom() -> getChatRoomList()
     fun searchChatRoom() {
-        MyApplication.realtime.child("chatRoomUser").orderByKey("User1").equalTo(userEmail)
-            .addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onCancelled(error: DatabaseError) {
+        MyApplication.realtime.child("chatRoomUser")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (chatRoomInfo in snapshot.children) {
+                        user1 = null
+                        user2 = null
+                        for (chatRoomUserData in chatRoomInfo.children) {
+                            if (chatRoomUserData.value == userEmail) {
+                                user1 = chatRoomUserData.value as String
+                            } else if (chatRoomUserData.value != userEmail) {
+                                user2 = chatRoomUserData.value as String
+                            }
+
+                            if (user1 != null && user2 != null) {
+                                if (user1 == userEmail) {
+                                    chatRoomId = chatRoomInfo.key as String
+                                    getChatRoomList(chatRoomId!!, user2!!)
+                                }
+                                break
+                            }
+                        }
+                    }
                 }
 
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    for (data in snapshot.children) {
-                        getChatRoomList(data.key.toString())
-                        Log.d("채팅방있음 !! ", data.key.toString())
-                    }
+                override fun onCancelled(error: DatabaseError) {
+                    Log.d("error", "failed")
                 }
             })
     }
 
-    fun getChatRoomList(chatRoomId: String) {
-        MyApplication.realtime.child("UserRoom").child(chatRoomId)
+    fun getChatRoomList(chatroomid: String, chatroomuser: String) {
+        MyApplication.realtime.child("UserRoom").child(chatroomid)
             .addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     var lastmessage = ""
-                    var sender = ""
+                    var sender = chatroomuser       //이름 및 이미지는 어댑터에서 sender로 처리하기 위해 상대방 이메일 지정해줌
                     var timestamp: Long = 0
                     for (data in snapshot.children) {
                         when (data.key) {
                             "lastmessage" -> lastmessage = data.value as String
                             "timestamp" -> timestamp = data.value as Long
-                            "sender" -> sender = data.value as String
                         }
-
                     }
-                    chatListDatas.add(UserRoom(lastmessage, timestamp, sender))
+                    chatListDatas.add(UserRoom(chatroomid, lastmessage, timestamp, sender))
+                    chatListDatas.sortByDescending { it.timestamp }     //시간 순으로 정렬
                     adapter.notifyDataSetChanged()
                 }
 
                 override fun onCancelled(error: DatabaseError) {
+                    Log.d("error", "failed")
                 }
             })
     }
