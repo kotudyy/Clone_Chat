9d02fd5fa6810d38004d92a445c10f32b20f46d2
cjyeon <cwd6372@gmail.com>
2022-01-02 10:16:05 +0900

    읽지 않은 메시지 개수 표시 부분 구현중

--- app/src/main/java/com/example/chatting/ChatFragment/RvItemChatAdapter.kt
+++ app/src/main/java/com/example/chatting/ChatFragment/RvItemChatAdapter.kt
@@ -1,16 +1,25 @@
 package com.example.chatting.ChatFragment
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.chatting.ChatRoomActivity
+import com.example.chatting.Model.Messages
 import com.example.chatting.Model.UserRoom
 import com.example.chatting.MyApplication
 import com.example.chatting.R
 import com.example.chatting.databinding.RvitemChatBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -30,7 +39,7 @@ class RvItemChatViewHolder(val binding: RvitemChatBinding) : RecyclerView.ViewHo
                 .whereEqualTo("email", data.sender)
                 .get()
                 .addOnSuccessListener { document ->
-                    for (field in document) binding.tvChatUsername.text = field["name"] as String
+                    for (field in document) tvChatUsername.text = field["name"] as String
                 }
 
             val imgRefProfile = MyApplication.storage.reference.child("${data.sender}/profile")
@@ -39,30 +48,98 @@ class RvItemChatViewHolder(val binding: RvitemChatBinding) : RecyclerView.ViewHo
                 .error(R.drawable.img_profile)
                 .into(ivChatProfile)
 
-            binding.tvChatPreviewmsg.text = data.lastmessage
+            tvChatPreviewmsg.text = data.lastmessage
 
             val year = SimpleDateFormat("yyyy")
             val date = SimpleDateFormat("MM.dd")
 
             //연도 체크
-            if(year.format(data.timestamp) == year.format(today)) {
+            if (year.format(data.timestamp) == year.format(today)) {
                 //오늘인 경우
-                if(date.format(data.timestamp) == date.format(today)) {
+                if (date.format(data.timestamp) == date.format(today)) {
                     val sdf = SimpleDateFormat("hh:mm")
-                    binding.tvChatTimestamp.text = sdf.format(data.timestamp)
+                    tvChatTimestamp.text = sdf.format(data.timestamp)
                 }
                 //어제인 경우
-                else if(date.format(data.timestamp) == date.format(yesterday)) {
-                    binding.tvChatTimestamp.text = "어제"
-                }
-                else {
+                else if (date.format(data.timestamp) == date.format(yesterday)) {
+                    tvChatTimestamp.text = "어제"
+                } else {
                     val sdf = SimpleDateFormat("MM월 dd일")
-                    binding.tvChatTimestamp.text = sdf.format(data.timestamp)
+                    tvChatTimestamp.text = sdf.format(data.timestamp)
                 }
             } else {
                 val sdf = SimpleDateFormat("yyyy.MM.dd")
-                binding.tvChatTimestamp.text = sdf.format(data.timestamp)
+                tvChatTimestamp.text = sdf.format(data.timestamp)
             }
+
+
+            // 채팅방별 안 읽은 메시지 개수
+            var myIdKey = ""
+            var lastVisitedTime: Long = 0
+
+            Log.d("@@chatRoomId", data.chatroomid.toString())
+            MyApplication.realtime.child("chatRoomUser").child(data.chatroomid.toString())
+                .get()
+                .addOnSuccessListener {
+                    for (data in it.children) {
+                        when (data.value) {
+                            MyApplication.auth.currentUser?.email -> myIdKey =
+                                data.key as String
+                        }
+                    }
+
+                    lastVisitedTime = getLastVisitedTime(data.chatroomid.toString(), myIdKey)
+
+                    getChatNum(data.chatroomid.toString(), lastVisitedTime)
+                }
+        }
+    }
+
+    // 채팅방 마지막 접속 시간 가져오는 함수
+    fun getLastVisitedTime(chatRoomId: String, idKey: String): Long {
+        var time: Long = 0
+
+        MyApplication.realtime.child("UserLastVisited").child(chatRoomId)
+            .get()
+            .addOnSuccessListener {
+                for (data in it.children) {
+                    when (data.key) {
+                        idKey -> time = data.value as Long
+                    }
+                    Log.d("@@lastVisitedTime", time.toString())
+                }
+            }
+
+        return time
+    }
+
+
+    // 안 읽은 메시지 수 체크하는 함수
+    fun getChatNum(chatRoomId: String, lastVisitedTime: Long) {
+        var chatNum = 0
+        MyApplication.realtime.child("Messages").child(chatRoomId).get().addOnSuccessListener {
+            var timestamp: Long = 0
+
+            for (data in it.children) {
+                for (data_time in data.children) {
+                    when (data_time.key) {
+                        "timestamp" -> timestamp = data_time.value as Long
+                    }
+
+//                    if (timestamp > lastVisitedTime) {
+//                    chatNum++
+//                    Log.d("@@chatNum++", chatNum.toString())
+//                }
+                }
+            }
+
+            if (chatNum == 0) {
+                binding.cardViewChatNum.visibility = View.INVISIBLE
+            } else {
+                binding.cardViewChatNum.visibility = View.VISIBLE
+                binding.tvChatNum.text = chatNum.toString()
+            }
+
         }
     }
 
