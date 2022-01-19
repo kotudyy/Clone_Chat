010ad68d715b7c85b51c5c445853b37d3f62ed73
dongk00 <dddongk00@gmail.com>
2022-01-19 10:55:06 +0900

    알림 이미지

--- app/src/main/java/com/example/chatting/Model/ServerMsg.kt
+++ app/src/main/java/com/example/chatting/Model/ServerMsg.kt
@@ -1,7 +1,10 @@
 package com.example.chatting.Model
 
+import android.net.Uri
+
 data class ServerMsg(
     var name: String,
     var text: String,
     var timestamp: Long,
+    var byteArray: ByteArray?
 )
